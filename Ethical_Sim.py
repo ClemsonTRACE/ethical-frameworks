5fa08e543d89154d94bb92dea4abbfe45791f43d
cflathm <ctflathmann@gmail.com>
2020-04-07 17:41:25 -0400

    Sim Base
    Basic Sim for Ethical, needs to have the process outlined

---
+++ Ethical_Sim.py
@@ -0,0 +1,96 @@
+import random
+import json
+import sys
+
+class Ethical_Sim:
+    dilemmas = [] #Dilemmas that the player will tackle
+    gifts = ["A Hat", "A Board Game", "A Sweater", "A New Bike", "A New Computer"]
+    dilemmasDone = [] #Dilemma list the player traversed
+    QUESTION_COUNT = None #Number of questions we want to ask
+    modifierTypes = ("P_Number", "T_Number", "H_Percent", "M_Percent", "L_Percent", "Result", "Gift")
+    relationOptions = ("Family Member(s)", "Friend(s)", "Elderly Stranger(s)", "Young Stranger(s)", "Stranger(s)")
+    results = ["Dead", "In Pain"]
+
+    def __init__(self, questionCount):
+        json_array = json.load(open("Dilemna.json"))
+        self.QUESTION_COUNT = questionCount
+
+        #Load list of dilemmas into memory
+        for item in json_array:
+            self.dilemmas.append(item)
+
+        #Generate initial node randomly
+        self.makeNextDilemma(random.randint(0,len(self.dilemmas)-1), random.randint(0,1))
+
+
+
+    #Make a decision on the current dilemma and pick the next one, this needs 
+    #to be separate form the sending of a dilemma due to the first node being 
+    #randomly generated
+    def makeNextDilemma(self, currentDilemma, decision):
+        nextDilemma = random.choice(self.dilemmas[currentDilemma]["target_"+str(decision)])
+        node = self.dilemmas[nextDilemma].copy()
+        for ind, mod in enumerate(node["Modifier_Types"]):
+            print(mod)
+            if mod == self.modifierTypes[0]: #People
+                node["Modifiers"].append(random.randint(0, 10))
+            elif mod == self.modifierTypes[1]: #Time (Days)
+                node["Modifiers"].append(random.randint(1, 10))
+            elif mod == self.modifierTypes[2]: #High Percent
+                node["Modifiers"].append(random.randint(66,101)/100.0)
+            elif mod == self.modifierTypes[3]: #Medium Percent
+                node["Modifiers"].append(random.randint(33,66)/100.0)
+            elif mod == self.modifierTypes[4]: #Low Percent
+                node["Modifiers"].append(random.randint(0,33)/100.0)
+            elif mod == self.modifierTypes[5]: #results
+                node["Modifiers"].append(random.choice(self.results))
+            elif mod == self.modifierTypes[6]: #Gift
+                node["Modifiers"].append(random.choice(self.gifts))
+            print(node["Modifiers"])
+            node["Description"] = node["Description"].replace("[M"+str(ind)+"]", str(node["Modifiers"][-1]))
+        
+        for relation in range(0, node["Relation_Count"]):
+            node["Relationships"].append(random.choice(self.relationOptions))
+            node["Description"] = node["Description"].replace("[relation_"+str(relation)+"]", node["Relationships"][-1])
+
+        self.dilemmasDone.append(node)
+
+    #return the current dilemma the player is doing, which should be the last one
+    def getCurrentDilemma(self):
+        return self.dilemmas[-1]
+
+    #Utilitarian reward, based on creating the most good, this is going to 
+    #look at helping the most people.  Life or death is valued at a 1, pain is 
+    #valued at a 0.5, the reward is calculated for how much good is made vs. 
+    #how much total good is available
+    def utilitarianReward(self, dilemma, decision):
+        
+
+    #The deontology reward is based on a strict act based deontology where 
+    #hard rules are set and not broken. These are scored with 0 for break
+    #and 1 for keep, these rules are:
+    #Do not Deliberately Kill
+    #Do not Deliberately Harm Others
+    #Do not Deliberately Harm Yourself
+    #Do not Steal
+    #Do not Lie
+    def deontologyReward(self, dilemma, decision):
+
+    
+    #Virtues ethics are based on common virtues that are seen in humans.  While 
+    #this study does not focus on every virtue, this is designed to act as a 
+    #representative base for what virtue ethics would look like.  Relavent Virtues 
+    #scored with 0 for ignore or 1 for prioritize include:
+    #Justice - Dealing fairly with others
+    #Liberality - The virtue of charity
+    #Friendliness - Be friendly to other
+    #Prioritize Relationships - emphasise family and friends 
+    #Courage - Aware of danger, but act
+    #Shame - Understand a moral error, but willing to risk
+    #Truthfulness - virtue of honesty
+    def virtueEthicsReward(self, dilemma, decision):
+        
+            
+    
+sim = Ethical_Sim(20)
+print(sim.dilemmasDone[-1])
